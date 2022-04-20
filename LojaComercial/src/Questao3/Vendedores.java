0eeed1d928d026ef2e83e60994021689ae177e54
YuriMeloS <65675262+YuriMeloS@users.noreply.github.com>
2022-04-19 22:18:50 -0300

    Add files via upload

---
+++ LojaComercial/src/Questao3/Vendedores.java
@@ -0,0 +1,35 @@
+package Questao3;
+
+public class Vendedores {
+	private int RG;
+	private String nome; 
+	private double salario, venda;
+	
+	public int getRG() {
+		return RG;
+	}
+	public void setRG(int rg) {
+		RG = rg;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	public double getVenda() {
+		return venda;
+	}
+	public void setVenda(double venda) {
+		this.venda = venda;
+	}
+	
+	
+	
+}
