c2e8d3ac6943ac007b76ee1cfb81b1f5635d0500
YuriMeloS <65675262+YuriMeloS@users.noreply.github.com>
2022-04-19 22:22:30 -0300

    Add files via upload

--- LojaComercial/src/Questao3/Administrativos.java
+++ LojaComercial/src/Questao3/Administrativos.java
@@ -1,5 +1,33 @@
 package Questao3;
 
 public class Administrativos {
-
+	private double salarioFixo;
+	private double horaExtra;
+	private int RG;
+	private String nome;
+	public double getSalarioFixo() {
+		return salarioFixo;
+	}
+	public void setSalarioFixo(double salarioFixo) {
+		this.salarioFixo = salarioFixo;
+	}
+	public double getHoraExtra() {
+		return horaExtra;
+	}
+	public void setHoraExtra(double horaExtra) {
+		this.horaExtra = horaExtra;
+	}
+	public int getRG() {
+		return RG;
+	}
+	public void setRG(int rG) {
+		RG = rG;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	} 
+	
 }
